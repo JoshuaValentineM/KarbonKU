2bb1c57726946a4fa99e604db0527bec56ca2a45
Jevon Christopher Loanda <jevon.loanda@gmail.com>
2024-07-29 14:09:49 +0700

    added facebook_sign_in method, added Auth Page UI

---
+++ lib/auth_page.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // The user canceled the sign-in
+        return;
+      }
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      if (user != null) {
+        // Successfully signed in
+        print('Google Sign-In successful: ${user.displayName}');
+      }
+    } catch (e) {
+      print('Google Sign-In error: $e');
+    }
+  }
+
+  Future<void> _signInWithApple() async {
+    // TODO: Implement Apple sign-in
+  }
+
+  Future<void> _signInWithFacebook() async {
+    try {
+      final LoginResult result = await FacebookAuth.instance.login(
+        permissions: ['email', 'public_profile'],
+      );
+
+      if (result.status == LoginStatus.success) {
+        final AccessToken accessToken = result.accessToken!;
+        final OAuthCredential facebookCredential = FacebookAuthProvider.credential(
+          accessToken.tokenString
+        );
+
+        final UserCredential userCredential = await _auth.signInWithCredential(facebookCredential);
+        final User? user = userCredential.user;
+
+        if (user != null) {
+          // Successfully signed in
+          print('Facebook Sign-In successful: ${user.displayName}');
+        }
+      } else {
+        // Handle login error
+        print('Facebook Sign-In error: ${result.message}');
+      }
+    } catch (e) {
+      print('Facebook Sign-In error: $e');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFEFFFF8), // Background color
+        ),
+        child: Stack(
+          children: [
+            // Top image
+            Positioned.fill(
+              top: 0,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  'assets/img/auth_header.png',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            // Centered white box
+            Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text(
+                      'DAFTAR/MASUK',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    const SizedBox(height: 20),
+                    _signInButton(
+                      onPressed: _signInWithGoogle,
+                      imagePath: 'assets/img/logo_google.png',
+                      text: 'Lanjutkan dengan Google',
+                    ),
+                    const SizedBox(height: 10),
+                    _signInButton(
+                      onPressed: _signInWithApple,
+                      imagePath: 'assets/img/logo_apple.png',
+                      text: 'Lanjutkan dengan Apple ID',
+                    ),
+                    const SizedBox(height: 10),
+                    _signInButton(
+                      onPressed: _signInWithFacebook,
+                      imagePath: 'assets/img/logo_facebook.png',
+                      text: 'Lanjutkan dengan Facebook',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton({
+    required VoidCallback onPressed,
+    required String imagePath,
+    required String text,
+  }) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5), // Add vertical margin here
+      child: SizedBox(
+        width: double.infinity,
+        child: OutlinedButton(
+          onPressed: onPressed,
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.black),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            backgroundColor: Colors.white,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                imagePath,
+                height: 40,
+                width: 45,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'auth_page.dart';
 import 'firebase_options.dart';
-import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,12 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'KarbonKU',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const AuthPage(),
     );
   }
 }
