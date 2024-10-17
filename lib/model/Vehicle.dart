b5fd5fa81e681c7d410e2d66303053377b29146b
Alfredjho <alfredjhonatan@gmail.com>
2024-10-17 20:22:07 +0700

    feat: calculator with dummy model, carbon report with dummy data

---
+++ lib/model/Vehicle.dart
@@ -0,0 +1,11 @@
+class Vehicle {
+  final String vehicleType;
+  final String vehicleName;
+  final int vehicleEmission;
+
+  Vehicle({
+    required this.vehicleType,
+    required this.vehicleName,
+    required this.vehicleEmission,
+  });
+}
