7f9ddb1b838b87e4a51f94d13c8063eee5cae8ec
Alfredjho <alfredjhonatan@gmail.com>
2024-11-09 10:30:57 +0700

    update feat: calculator with db

--- lib/view/calculator_page.dart
+++ lib/view/calculator_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:karbonku/model/Vehicle.dart';
 import 'package:karbonku/view/custom_bottom_nav.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home_page.dart';
 import 'profile_page.dart';
 import 'education_page.dart';
@@ -18,66 +19,92 @@ class _CalculatorPageState extends State<CalculatorPage> {
   final user = FirebaseAuth.instance.currentUser;
   int _selectedIndex = 1;
   int currentYear = DateTime.now().year; // Current year
-
+  final FirebaseAuth auth = FirebaseAuth.instance;
   double carbonTaxRate = 30; // Tarif pajak karbon per kilogram CO2e
 
-  Map<int, List<Vehicle>> vehicleDataByYear = {
-    2023: [
-      Vehicle(
-          vehicleType: 'Motor',
-          vehicleName: 'Honda Beat',
-          vehicleEmission: 600),
-      Vehicle(
-          vehicleType: 'Mobil',
-          vehicleName: 'Toyota Creta',
-          vehicleEmission: 1000),
-    ],
-    2024: [
-      Vehicle(
-          vehicleType: 'Motor',
-          vehicleName: 'Yamaha NMAX',
-          vehicleEmission: 500),
-      Vehicle(
-          vehicleType: 'Mobil',
-          vehicleName: 'Daihatsu Ayla',
-          vehicleEmission: 800),
-      Vehicle(
-          vehicleType: 'Mobil', vehicleName: 'Wuling', vehicleEmission: 400),
-    ],
-    // Future data for 2025 with no data
-    2025: [],
-  };
-
-  // Initial vehicles based on the current year
-  List<Vehicle> vehicles = [];
+  List<Map<String, dynamic>> vehicles = [];
 
   @override
   void initState() {
     super.initState();
-    vehicles = vehicleDataByYear[currentYear] ?? [];
+    _loadVehicleData();
+  }
+
+  // Function to load vehicle data from Firestore
+  Future<void> _loadVehicleData() async {
+    final vehiclesRef = FirebaseFirestore.instance
+        .collection('vehicles')
+        .where('userId', isEqualTo: auth.currentUser!.uid);
+    final querySnapshot = await vehiclesRef.get();
+
+    List<Map<String, dynamic>> tempVehicles = [];
+
+    for (var doc in querySnapshot.docs) {
+      var vehicleData = doc.data() as Map<String, dynamic>;
+      print("Data Kendaraan: $vehicleData"); // Log data kendaraan
+
+      if (vehicleData.containsKey('tracks')) {
+        var tracks = vehicleData['tracks'] as List<dynamic>;
+
+        // Filter tracks by year
+        var filteredTracks = tracks.where((track) {
+          var trackDate;
+
+          // Handle both Timestamp and String formats
+          if (track['date'] is Timestamp) {
+            trackDate = (track['date'] as Timestamp).toDate();
+          } else if (track['date'] is String) {
+            trackDate = DateTime.tryParse(track['date']);
+          }
+
+          // Filter by the current year
+          return trackDate != null && trackDate.year == currentYear;
+        }).toList();
+
+        if (filteredTracks.isNotEmpty) {
+          tempVehicles.add({
+            'vehicleName': vehicleData['vehicleName'],
+            'vehicleType': vehicleData['vehicleType'],
+            'tracks': filteredTracks,
+          });
+        }
+      } else {
+        print("Tidak ada 'tracks' dalam dokumen ${doc.id}");
+      }
+    }
+
+    setState(() {
+      vehicles = tempVehicles;
+    });
+
+    print(
+        'Total Pajak Karbon: Rp ${calculateTotalTax().toStringAsFixed(2)}'); // Hitung total pajak setelah data terisi
   }
 
   double calculateTotalTax() {
     double totalTax = 0;
     for (var vehicle in vehicles) {
-      totalTax += vehicle.vehicleEmission * carbonTaxRate;
+      for (var track in vehicle['tracks']) {
+        print(
+            'Track Carbon Emission: ${track['carbon_emission']}'); // Log tambahan
+        totalTax += (track['carbon_emission'] / 1000) * carbonTaxRate;
+      }
     }
+    print('Total Carbon Tax: $totalTax'); // Log total pajak karbon
     return totalTax;
   }
 
   void incrementYear() {
     setState(() {
       currentYear++;
-      // Update vehicles based on the new year
-      vehicles = vehicleDataByYear[currentYear] ?? [];
+      _loadVehicleData();
     });
   }
 
   void decrementYear() {
     setState(() {
       currentYear--;
-      // Update vehicles based on the new year
-      vehicles = vehicleDataByYear[currentYear] ?? [];
+      _loadVehicleData();
     });
   }
 
@@ -176,7 +203,7 @@ class _CalculatorPageState extends State<CalculatorPage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             // First box: Year with arrows on left and right
-            const SizedBox(height: 8),
+            const SizedBox(height: 24),
             Center(
               child: Container(
                 width: width, // Using 85% of the screen width
@@ -250,150 +277,186 @@ class _CalculatorPageState extends State<CalculatorPage> {
               ),
               child: SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    // Header row for first and second columns
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Penghitungan Pajak',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        const Spacer(),
-                        Image.asset(
-                          'assets/img/Leaf_fill.png',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-
-                    if (vehicles.isEmpty)
-                      const Center(
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 112.0),
-                          child: Text(
-                            'Belum ada data untuk tahun ini.',
-                            style: TextStyle(fontSize: 16),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      )
-                    else ...[
-                      // Loop through vehicles to display dynamic data
-                      for (var vehicle in vehicles) ...[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Image.asset(
-                              vehicle.vehicleType == 'Motor'
-                                  ? 'assets/img/MotorbikeIconFill.png'
-                                  : 'assets/img/CarIconFill.png',
-                              width: 24,
-                              height: 24,
-                            ),
-                            const SizedBox(width: 16),
-                            Text(
-                              vehicle.vehicleName,
-                              style: const TextStyle(
-                                  fontFamily: 'Poppins', fontSize: 16),
-                            ),
-                            const Spacer(),
-                            const SizedBox(width: 8),
-                            Text(
-                              '${vehicle.vehicleEmission}kg',
-                              style: const TextStyle(
-                                  fontFamily: 'Poppins', fontSize: 16),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                      ],
-
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // Header row for first and second columns
                       Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           const Text(
-                            'Tarif Pajak Karbon',
-                            style:
-                                TextStyle(fontFamily: 'Poppins', fontSize: 16),
-                          ),
-                          const SizedBox(width: 8),
-                          GestureDetector(
-                            onTap: _showWarningBottomSheet,
-                            child: Image.asset(
-                              'assets/img/informationIcon.png',
-                              width: 14,
-                              height: 14,
+                            'Penghitungan Pajak',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
                           const Spacer(),
-                          const SizedBox(width: 8),
-                          Text(
-                            'Rp${carbonTaxRate.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                                fontFamily: 'Poppins', fontSize: 16),
+                          Image.asset(
+                            'assets/img/Leaf_fill.png',
+                            width: 24,
+                            height: 24,
                           ),
                         ],
                       ),
                       const SizedBox(height: 16),
 
-                      // Pajak per kendaraan
-                      for (var vehicle in vehicles) ...[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Pajak ${vehicle.vehicleName}',
-                              style: const TextStyle(
-                                  fontFamily: 'Poppins', fontSize: 16),
+                      if (vehicles.isEmpty)
+                        const Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 112.0),
+                            child: Text(
+                              'Belum ada data untuk tahun ini.',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.center,
                             ),
-                            const Spacer(),
-                            const SizedBox(width: 8),
-                            Text(
-                              'Rp${(vehicle.vehicleEmission * carbonTaxRate).toStringAsFixed(2)}',
-                              style: const TextStyle(
-                                  fontFamily: 'Poppins', fontSize: 16),
+                          ),
+                        )
+                      else
+                        Column(
+                          children: [
+                            // Menampilkan data kendaraan terlebih dahulu
+                            ...vehicles.map((vehicle) {
+                              double totalEmissions = 0;
+
+                              // Menghitung total emisi dari tracks
+                              vehicle['tracks'].forEach((track) {
+                                totalEmissions += track['carbon_emission'];
+                              });
+
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Image.asset(
+                                          vehicle['vehicleType'] == 'motor'
+                                              ? 'assets/img/MotorbikeIconFill.png'
+                                              : 'assets/img/CarIconFill.png',
+                                          width: 24,
+                                          height: 24,
+                                        ),
+                                        const SizedBox(width: 12),
+                                        Text(
+                                          vehicle['vehicleName'],
+                                          style: const TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        const SizedBox(width: 8),
+                                        Text(
+                                          '${(totalEmissions / 1000).toStringAsFixed(2)} kg', // Convert to kg
+                                          style: const TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+
+                            // Tarif Pajak Karbon
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Tarif Pajak Karbon',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins', fontSize: 16),
+                                ),
+                                SizedBox(width: 8),
+                                GestureDetector(
+                                  onTap: _showWarningBottomSheet,
+                                  child: Image.asset(
+                                    'assets/img/informationIcon.png',
+                                    width: 14,
+                                    height: 14,
+                                  ),
+                                ),
+                                Spacer(),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Rp${carbonTaxRate.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins', fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 8),
+// Menampilkan Pajak per kendaraan setelah Tarif Pajak Karbon
+                            ...vehicles.map((vehicle) {
+                              double totalEmissions = 0;
+
+                              // Menghitung total emisi dari tracks
+                              vehicle['tracks'].forEach((track) {
+                                totalEmissions += track['carbon_emission'];
+                              });
+
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Pajak ${vehicle['vehicleName']}',
+                                      style: const TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Rp${((totalEmissions / 1000) * carbonTaxRate).toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+
+                            const SizedBox(height: 8),
+
+                            // Menampilkan Total Pajak Karbon di bagian bawah daftar kendaraan
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Total',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins', fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 8),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Rp${calculateTotalTax().toStringAsFixed(2)}',
+                                  style: const TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xFF66D6A6),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        const SizedBox(height: 16),
-                      ],
-
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Total',
-                            style:
-                                TextStyle(fontFamily: 'Poppins', fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 8),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Rp${calculateTotalTax().toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 24,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xFF66D6A6),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ],
-                ),
+                    ]),
               ),
             ),
 
