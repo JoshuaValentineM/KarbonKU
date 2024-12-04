07fdd20c33a7719deec23f72c6e47a44d0fdf3d4
JoshuaValentineM <joshuavalentinem@gmail.com>
2024-12-04 14:48:10 +0700

    update feat: carbon report widget and popup

--- lib/model/Vehicle.dart
+++ lib/model/Vehicle.dart
@@ -1,13 +1,23 @@
 class Vehicle {
   final String vehicleType;
   final String vehicleName;
-  final int vehicleEmission;
-  final int? vehicleTravel;
+  final double vehicleEmission;
+  final double vehicleTravel;
 
   Vehicle({
     required this.vehicleType,
     required this.vehicleName,
     required this.vehicleEmission,
-    this.vehicleTravel,
+    required this.vehicleTravel,
   });
+
+  // Method untuk membuat instance dari Map (Firestore)
+  factory Vehicle.fromMap(Map<String, dynamic> map) {
+    return Vehicle(
+      vehicleType: map['vehicleType'] ?? 'Unknown',
+      vehicleName: map['vehicleName'] ?? 'Unknown',
+      vehicleEmission: map['vehicleEmission']?.toDouble() ?? 0.0,
+      vehicleTravel: map['vehicleTravel']?.toDouble() ?? 0.0,
+    );
+  }
 }
